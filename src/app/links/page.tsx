7c0853e82b461446d02e1c68f3d52d07c0b2be41
starchcode <mrdave67@gmail.com>
2025-02-19 22:10:42 +0000

    links route added

---
+++ src/app/links/page.tsx
@@ -0,0 +1,7 @@
+export default function Links() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen text-center'>
+      <h1 className='text-4xl'>Links will be added here</h1>
+    </div>
+  )
+}
